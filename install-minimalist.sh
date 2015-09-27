d8b0932f704a6734ee2b2177c19246e61bcc846f
Dênis Araújo da Silva <silvadenisaraujo@gmail.com>
2015-09-26 21:11:14 -0300

    Corrected installation scripts

--- install-minimalist.sh
+++ install-minimalist.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+echo "#############################################################"
+echo "#                Installing MINIMALIST                      #"
+echo "#############################################################"
 
 MINIMALIST_HOME=$1
 
 #if the parameter is not set, save on user home
 if [[ -z $MINIMALIST_HOME ]]; then
-   MINIMALIST_HOME=~
+   echo "No parameters passed, installing MINIMALIST on $HOME"
+   MINIMALIST_HOME=$HOME
 fi
 
 #Download minimalist to temp folder
@@ -15,13 +19,17 @@ wget http://www1.cs.columbia.edu/~nowick/asynctools/minimalist/core27/minimalist
 tar -xf minimalist2.0-linux.tgz -C $MINIMALIST_HOME
 
 #Set parameters to bashrc
-cat << EOT >> ~/.bashrc
+cat << EOT >> $HOME/.bashrc
+#Minimalist exports
 export MINIMALIST=$MINIMALIST_HOME/MINIMALIST
 export PATH=$MINIMALIST/bin:$MINIMALIST/Linux-X86/bin:$PATH
 export LD_LIBRARY_PATH=$MINIMALIST/Linux-X86/lib:$LD_LIBRART_PATH
 EOT
 
-
+#Necessary for using minimalist
+sudo apt-get update
 sudo apt-get -y install gv graphviz
 
-echo "MINIMALIST installed successfully!"
+echo "############################################################"
+echo "#           MINIMALIST installed successfully!             #"
+echo "############################################################"
